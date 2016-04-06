8fd739a6db316d18493521eaf01f025ff2150611
Asjad Hassan Malick <asjad.malick259@gmail.com>
2016-04-06 11:47:45 -0600

    OK MAJOR CHANGES
    Grennhouse thread modifes the temp, soil and humid according to the info
    presented in the first tab. The other uses the info on second tab

--- GreenHouseThread.java
+++ GreenHouseThread.java
@@ -2,27 +2,16 @@ package Assignment5;
 
 public class GreenHouseThread implements Runnable
 {
-	//Initialize a runnable tempthread with these
-	private double tempUpperLimit;
-	private double tempLowerLimit;
 	
-	//Initialize a runnable humidthread with these
-	private double humidUpperLimit;
-	private double humidLowerLimit;
-	
-	//Initilaize a runnable soilthread with these
-	private double soilUpperLimit;
-	private double soilLowerLimit;
-	
-	private boolean furnON = false;
-	private boolean sprinkON = false;
-	private boolean humidON = false;
+	private double tempRate = 0;
+	private double soilRate = 0;
+	private double humidRate = 0;
 	
 	private double temperature = 0;
 	private double soilMoisture = 0;
 	private double humidity = 0;
 	
-	public GreenHouseThread(double tUL, double tLL, double hUL, double hLL, double sUL, double sLL)
+	public GreenHouseThread()
 	{
 		super();
 	}
@@ -30,21 +19,6 @@ public class GreenHouseThread implements Runnable
 	public void run() 
 	{
 		//make the threads all run here
-	}
-	
-	public boolean isFurnON()
-	{
-		return furnON;
-	}
-	
-	public boolean isSprinkON()
-	{
-		return sprinkON;
-	}
-	
-	public boolean isHumidON()
-	{
-		return humidON;
 	}
 	
 	public double currentTemp()
--- TempThread.java
+++ TempThread.java
@@ -1,5 +1,31 @@
 package Assignment5;
 
-public class TempThread {
+public class TempThread implements Runnable
+{
+	private double rate;
+	
+	private double upperLimit;
+	private double lowerLimit;
+	
+	private double target;
+	
+	private double current;
+	
+	public TempThread(double cTemp, double UL, double LL, double hr, double cr, double hr)
+	{
+		upperLimit = UL;
+		lowerLimit = LL;
+		target = (UL - 3);
+		current = cTemp;
+	}
+	public double current()
+	{
+		return current;
+	}
+
+	public void run() 
+	{	
+		
+	}
 
 }
